45201df237dc5858c44dadee1813b6fb5a109060
Talha1337 <talhajaved@hotmail.co.uk>
2026-03-13 15:07:56 +0000

    refactored for functions split

--- postcode_cli.py
+++ postcode_cli.py
@@ -4,6 +4,33 @@ from argparse import ArgumentParser
 from postcode_functions import validate_postcode, get_postcode_completions
 
 
+def apply_operation(validation: bool, postcode: str) -> None:
+    if validation:
+        return output_validation(postcode)
+    return output_postcodes(postcode)
+
+
+def output_validation(postcode: str) -> None:
+    """print to terminal the validation of the postcode."""
+    if validate_postcode(postcode):
+        print(f"{postcode} is a valid postcode.")
+    else:
+        print(f"{postcode} is not a valid postcode.")
+
+
+def output_postcodes(postcode: str) -> None:
+    """print to terminal the postcode completions."""
+    possible_postcodes = get_postcode_completions(postcode)
+    if not possible_postcodes:
+        print(f"No matches for {postcode}.")
+        return None
+    i = 0
+    while i < min(len(possible_postcodes), 5):
+        print(possible_postcodes[i].upper().strip())
+        i += 1
+    return None
+
+
 if __name__ == "__main__":
     arg_parser = ArgumentParser()
     arg_parser.add_argument(
@@ -13,16 +40,4 @@ if __name__ == "__main__":
     args = arg_parser.parse_args()
     postcode = args.postcode.upper().strip()
     validate = True if args.mode == "validate" else False
-    if validate:
-        if validate_postcode(postcode):
-            print(f"{postcode} is a valid postcode.")
-        else:
-            print(f"{postcode} is not a valid postcode.")
-    else:
-        possible_postcodes = get_postcode_completions(postcode)
-        if not possible_postcodes:
-            print(f"No matches for {postcode}.")
-        i = 0
-        while i < min(len(possible_postcodes), 5):
-            print(possible_postcodes[i].upper().strip())
-            i += 1
+    apply_operation(validation=validate, postcode=postcode)
